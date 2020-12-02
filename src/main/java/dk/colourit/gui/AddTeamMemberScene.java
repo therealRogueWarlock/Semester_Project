83de96f707a3aaf2b9e105820146084e5f9887f4
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-02 17:20:12 +0100

    addTeamMemberScene.fxml now opens as a pop-up window and terminates when clicking "Add"

--- src/main/java/dk/colourit/gui/AddTeamMemberScene.java
+++ src/main/java/dk/colourit/gui/AddTeamMemberScene.java
@@ -1,12 +1,17 @@
 package dk.colourit.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AddTeamMemberScene {
     @FXML
+    private Button addTeamMemberButton;
+
+    @FXML
     private void addTeamMember() throws IOException {
-        ColourItGui.setRoot("primary");
+        ((Stage) addTeamMemberButton.getScene().getWindow()).close(); // Get's the Window the button is in, and casts to a Stage, which can be closed with .close()
     }
 }
--- src/main/java/dk/colourit/gui/SecondaryController.java
+++ src/main/java/dk/colourit/gui/SecondaryController.java
@@ -1,12 +1,26 @@
 package dk.colourit.gui;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        ColourItGui.setRoot("addTeamMemberScene");
+        Scene addTeamMemberScene = new Scene(loadFXML("addTeamMemberScene"));
+        Stage stage = new Stage();
+
+        stage.setScene(addTeamMemberScene);
+        stage.show();
     }
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ColourItGui.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
 }
