921d4cb1d1f076da2da3f25070b4ed9ba8877251
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-02 15:59:10 +0100

    addTeamMemberScene.fxml sketch

--- src/main/java/dk/colourit/AddTeamMemberScene.java
+++ src/main/java/dk/colourit/AddTeamMemberScene.java
@@ -1,4 +1,12 @@
 package dk.colourit;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class AddTeamMemberScene {
+    @FXML
+    private void addTeamMember() throws IOException {
+        App.setRoot("primary");
+    }
 }
