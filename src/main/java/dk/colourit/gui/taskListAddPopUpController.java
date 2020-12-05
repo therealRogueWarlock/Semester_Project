25389f56ddff26f3c5b5a11b90d74ca72635e3a7
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-05 19:51:08 +0100

    Made the taskListAddPopUp scene close upon adding a task

--- src/main/java/dk/colourit/gui/taskListAddPopUpController.java
+++ src/main/java/dk/colourit/gui/taskListAddPopUpController.java
@@ -1,4 +1,19 @@
 package dk.colourit.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class taskListAddPopUpController {
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private void addTask() {
+        //TODO: STORE TASK
+
+        ((Stage) confirmButton.getScene().getWindow()).close(); // Get's the Window the button is in, and casts to a Stage, which can be closed with .close()
+
+    }
 }
