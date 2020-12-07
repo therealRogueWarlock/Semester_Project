9c5a97b488c71e5817af7e28c79bd62d796581c5
Nivq <30695715+Nivq@users.noreply.github.com>
2020-12-07 11:43:26 +0100

    Controllers, buttons added
    Fxml edited

--- src/main/java/dk/colourit/gui/scrumClickOnTaskController.java
+++ src/main/java/dk/colourit/gui/scrumClickOnTaskController.java
@@ -1,4 +1,22 @@
 package dk.colourit.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class scrumClickOnTaskController {
+
+  @FXML private Label taskName;
+  @FXML private Label totalTimeSpent;
+  @FXML private CheckBox taskFinished;
+  @FXML private TextArea documentationText;
+  @FXML private TextField timeSpent;
+  @FXML private Button logSpentTime;
+  /*
+  Mangler noget til TableView
+  Team Member
+  Time Spent
+   */
+  @FXML private Button confirm;
+  @FXML private Button reject;
+
 }
