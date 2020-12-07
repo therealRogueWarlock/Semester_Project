11cdd3962129565e4f7befbe320da5a46a2935ec
Andreas Y Andersen <andreasyoungandersen@gmail.com>
2020-12-07 14:22:07 +0100

    Cant remember what I changed

--- src/main/java/dk/colourit/gui/PrimaryController.java
+++ src/main/java/dk/colourit/gui/PrimaryController.java
@@ -2,14 +2,18 @@ package dk.colourit.gui;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
+	public Button primaryButton;
+	public Button loginButton1;
 
-    @FXML
+	@FXML
     private void switchToSecondary() throws IOException {
         ColourItGui.setRoot("secondary");
     }
+
     @FXML
     private void switchToLogin() throws IOException {
         ColourItGui.setRoot("loginScreen");
--- src/main/java/dk/colourit/mediator/ColourITProjectManagement.java
+++ src/main/java/dk/colourit/mediator/ColourITProjectManagement.java
@@ -8,98 +8,98 @@ import dk.colourit.model.TeamMemberList;
 public class ColourITProjectManagement
 {
 
-  private ProjectList projectList;
+	private ProjectList projectList;
 
-  private TeamMemberList teamMemberList;
+	private TeamMemberList teamMemberList;
 
-  private ColourItFileHandler colourItFileHandler;
+	private ColourItFileHandler colourItFileHandler;
 
-  public ColourITProjectManagement()
-  {
+	public ColourITProjectManagement()
+	{
 
-  }
+	}
 
-  public void selectUserRole()
-  {
+	public void selectUserRole()
+	{
 
-  }
+	}
 
-  public void createProject()
-  {
+	public void createProject()
+	{
 
-  }
+	}
 
-  public void deleteProject(String projectName)
-  {
+	public void deleteProject(String projectName)
+	{
 
-  }
+	}
 
-  public void addMemberToProject(String projectName, String memberName,
-      int memberRole)
-  {
+	public void addMemberToProject(String projectName, String memberName,
+		int memberRole)
+	{
 
-  }
+	}
 
-  public void removeMemberFromProject(String projectName, String memberName)
-  {
+	public void removeMemberFromProject(String projectName, String memberName)
+	{
 
-  }
+	}
 
-  public void assignRoleToTeamMember(String projectName, String memberName,
-      String memberRole)
-  {
+	public void assignRoleToTeamMember(String projectName, String memberName,
+		String memberRole)
+	{
 
-  }
+	}
 
-  public void taskDocumentation(String projectName, String requirementName,
-      String taskName, int hours, MyDate date, String memberName)
-  {
+	public void taskDocumentation(String projectName, String requirementName,
+		String taskName, int hours, MyDate date, String memberName)
+	{
 
-  }
+	}
 
-  public void finishTask(String taskName)
-  {
+	public void finishTask(String taskName)
+	{
 
-  }
+	}
 
-  public void setRequirementReady(String requirementName, boolean ready)
-  {
+	public void setRequirementReady(String requirementName, boolean ready)
+	{
 
-  }
+	}
 
-  public void setRequirementApproval(String requirementName, boolean approval)
-  {
+	public void setRequirementApproval(String requirementName, boolean approval)
+	{
 
-  }
+	}
 
-  public void addRequirement(String requirementName)
-  {
+	public void addRequirement(String requirementName)
+	{
 
-  }
+	}
 
-  public void removeRequirement(String requirementName)
-  {
+	public void removeRequirement(String requirementName)
+	{
 
-  }
+	}
 
-  public void setRequirementPriority(String requirementName, int priority)
-  {
+	public void setRequirementPriority(String requirementName, int priority)
+	{
 
-  }
+	}
 
-  public void addTask(String taskName)
-  {
+	public void addTask(String taskName)
+	{
 
-  }
+	}
 
-  public void editTask(String taskName)
-  {
+	public void editTask(String taskName)
+	{
 
-  }
+	}
 
-  public void removeTask(int taskName)
-  {
+	public void removeTask(int taskName)
+	{
 
-  }
+	}
 
 }
