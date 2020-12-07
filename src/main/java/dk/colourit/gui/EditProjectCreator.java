748d587621450aede4de67e7f6511e1e0c9f0f8f
Frederik <frederik.m.hansen@gmail.com>
2020-12-07 12:12:41 +0100

    EditProjectCreator update

--- src/main/java/dk/colourit/gui/EditProjectCreator.java
+++ src/main/java/dk/colourit/gui/EditProjectCreator.java
@@ -1,5 +1,43 @@
 package dk.colourit.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class EditProjectCreator {
+
+  @FXML public Button editButton;
+  @FXML public Button addTeamMemberButton;
+  @FXML public Button deleteProjectButton;
+  @FXML public Button removeTeamMemberButton;
+  @FXML public ChoiceBox teamMemberList;
+  public TableView requirementTable;
+  public TableColumn requirementNameColumn;
+  public TableView teamMemberTable;
+  public TableColumn teamMemberNameColumn;
+  public TableColumn IDNumberColumn;
+  public TableColumn roleColumn;
+
+  @FXML private void editButton() {
+
+  }
+
+  @FXML private void addTeamMemberButton() {
+
+  }
+
+  @FXML private void deleteProjectButton(){
+
+  }
+
+  @FXML private void removeTeamMemberButton(){
+
+  }
+
+  @FXML private void teamMemberList() {
+
+  }
 
 }
