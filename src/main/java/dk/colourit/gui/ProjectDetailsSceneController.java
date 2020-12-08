5e91ca8c027e8beb4317ed1f60c7a0194c56d165
marker99 <frederik.m.hansen@gmail.com>
2020-12-08 13:01:18 +0100

    slettet 3 overflødige controllere

--- src/main/java/dk/colourit/gui/ProjectDetailsSceneController.java
+++ src/main/java/dk/colourit/gui/ProjectDetailsSceneController.java
@@ -1,6 +1,19 @@
 package dk.colourit.gui;
 
+import dk.colourit.model.Project;
+import dk.colourit.model.Requirement;
+import dk.colourit.model.TeamMember;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ProjectDetailsSceneController extends Controller
 {
@@ -9,23 +22,18 @@ public class ProjectDetailsSceneController extends Controller
   public Button addRequirementButton;
   public ChoiceBox requirementList;
   public Button removeRequirementButton;
-  public TableView requirementTable;
+  public TableView<Requirement> requirementTable;
   public TableColumn requirementPriorityColumn;
-  public TableColumn requirementNameColumn;
+  public TableColumn<Requirement, String> requirementNameColumn;
   public TableColumn requirementStatusColumn;
   public Button addTeamMemberButton;
   public Button deleteProjectButton;
   public ChoiceBox teamMemberList;
   public Button removeTeamMemberButton;
-  public TableView teamMemberTable;
-  public TableColumn teamMemberNameColumn;
-  public TableColumn idNumberColumn;
-  public TableColumn roleColumn;
-
-  @Override public void init()
-  {
-
-  }
+  public TableView<TeamMember> teamMemberTable;
+  public TableColumn<TeamMember, String> teamMemberNameColumn;
+  public TableColumn<TeamMember, Integer> idNumberColumn;
+  public TableColumn<TeamMember, Integer> roleColumn;
 
   public void editButton()
   {
@@ -47,8 +55,18 @@ public class ProjectDetailsSceneController extends Controller
   {
   }
 
-  public void addTeamMemberButton()
+  public void addTeamMemberButton() throws IOException
   {
+    Scene addTeamMemberScene = new Scene(loadFXML("addTeamMemberScene"));
+    Stage stage = new Stage();
+
+    stage.setScene(addTeamMemberScene);
+    stage.show();
+  }
+  private static Parent loadFXML(String fxml) throws IOException
+  {
+    FXMLLoader fxmlLoader = new FXMLLoader(ColourItGui.class.getResource(fxml + ".fxml"));
+    return fxmlLoader.load();
   }
 
   public void deleteProjectButton()
@@ -62,6 +80,33 @@ public class ProjectDetailsSceneController extends Controller
   public void removeTeamMemberButton()
   {
   }
+
+  @Override public void init()
+  {
+    Project project = (Project) getObjectHolderForInit();
+    ArrayList<Requirement> requirements = project.getRequirementList().getRequirements();
+    ObservableList<Requirement> observableRequirementList =  FXCollections.observableArrayList();
+
+    observableRequirementList.addAll(requirements);
+
+
+    requirementNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+    requirementTable.setItems(observableRequirementList);
+
+
+    ArrayList<TeamMember> teamMembers = project.getTeamMemberList().getTeamMembers();
+    ObservableList<TeamMember> observableTeamMembers =  FXCollections.observableArrayList();
+
+    observableTeamMembers.addAll(teamMembers);
+
+    teamMemberNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+    idNumberColumn.setCellValueFactory(new PropertyValueFactory<>("employeeNumber"));
+    teamMemberNameColumn.setCellValueFactory(new PropertyValueFactory<>("role"));
+  }
+
+
+
 }
 
 
