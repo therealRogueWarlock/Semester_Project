efa8226382a8b7dd0afb3e72f5f4c4522bc61eff
marker99 <frederik.m.hansen@gmail.com>
2020-12-08 12:51:39 +0100

    lavet ProjectDetailsSceneController og projectDetailsScene.fxml

---
+++ src/main/java/dk/colourit/gui/ProjectDetailsSceneController.java
@@ -0,0 +1,67 @@
+package dk.colourit.gui;
+
+import javafx.scene.control.*;
+
+public class ProjectDetailsSceneController extends Controller
+{
+  public Button editButton;
+  public TextField addRequirementInput;
+  public Button addRequirementButton;
+  public ChoiceBox requirementList;
+  public Button removeRequirementButton;
+  public TableView requirementTable;
+  public TableColumn requirementPriorityColumn;
+  public TableColumn requirementNameColumn;
+  public TableColumn requirementStatusColumn;
+  public Button addTeamMemberButton;
+  public Button deleteProjectButton;
+  public ChoiceBox teamMemberList;
+  public Button removeTeamMemberButton;
+  public TableView teamMemberTable;
+  public TableColumn teamMemberNameColumn;
+  public TableColumn idNumberColumn;
+  public TableColumn roleColumn;
+
+  @Override public void init()
+  {
+
+  }
+
+  public void editButton()
+  {
+  }
+
+  public void addRequirementInput()
+  {
+  }
+
+  public void addRequirementButton()
+  {
+  }
+
+  public void requirementList()
+  {
+  }
+
+  public void removeRequirementButton()
+  {
+  }
+
+  public void addTeamMemberButton()
+  {
+  }
+
+  public void deleteProjectButton()
+  {
+  }
+
+  public void teamMemberList()
+  {
+  }
+
+  public void removeTeamMemberButton()
+  {
+  }
+}
+
+
