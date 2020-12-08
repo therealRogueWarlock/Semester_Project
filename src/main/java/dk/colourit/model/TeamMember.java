0beee5bfa53b764e74652dd5731934e31e2945c8
Andreas Y Andersen <andreasyoungandersen@gmail.com>
2020-12-08 16:36:11 +0100

    TeamMember throws outOfRangeException if role doesn't fit the range

--- src/main/java/dk/colourit/model/TeamMember.java
+++ src/main/java/dk/colourit/model/TeamMember.java
@@ -1,71 +1,98 @@
 package dk.colourit.model;
 
-public class TeamMember {
+public class TeamMember
+{
 
-    private String name;
-    private int employeeNumber;
-    private int role;
-    /*
-    0 = Team Member
-    1 = Product Owner
-    2 = Scrum Master
-    3 = Project Creator
-     */
-    private MyDate birthday;
+	private String name;
+	private int employeeNumber;
+	private int role;
+	/*
+	0 = Team Member
+	1 = Product Owner
+	2 = Scrum Master
+	3 = Project Creator
+	 */
+	private MyDate birthday;
 
-    public TeamMember(String name, int employeeNumber, MyDate birthday) {
-        this.name = name;
-        this.employeeNumber = employeeNumber;
-        this.birthday = birthday;
-        setRole(0);
-    }
+	public TeamMember(String name, int employeeNumber, MyDate birthday)
+	{
+		this.name = name;
+		this.employeeNumber = employeeNumber;
+		this.birthday = birthday;
+		setRole(0);
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName()
+	{
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
-    public int getEmployeeNumber() {
-        return employeeNumber;
-    }
+	public int getEmployeeNumber()
+	{
+		return employeeNumber;
+	}
 
-    public void setEmployeeNumber(int number) {
-        employeeNumber = number;
-    }
+	public void setEmployeeNumber(int number)
+	{
+		employeeNumber = number;
+	}
 
-    public MyDate getBirthday() {
-        return birthday;
-    }
+	public MyDate getBirthday()
+	{
+		return birthday;
+	}
 
-    public void setBirthday(MyDate myDate) {
-        birthday = myDate.copy();
-    }
+	public void setBirthday(MyDate myDate)
+	{
+		birthday = myDate.copy();
+	}
 
-    public String getRole() {
-        switch (role) {
-            case (1):
-                return "Product Owner";
-            case (2):
-                return "Scrum Master";
-            case (3):
-                return "Project Creator";
-            default:
-                return "Team Member";
-        }
-    }
+	public String getRole()
+	{
+		switch (role)
+		{
+			case (1):
+				return "Product Owner";
+			case (2):
+				return "Scrum Master";
+			case (3):
+				return "Project Creator";
+			default:
+				return "Team Member";
+		}
+	}
 
-    public void setRole(int role) {
-        this.role = role;
-        //TODO: Sørge for at vi maks kan have en rolle på 3, og mindst 0. - Throw en error, hvis den er udenfor denne ramme! - SBT
-    }
+	public void setRole(int role)
+	{
+		this.role = role;
+		if (role < 0 || role > 4)
+		{
+			throw new OutOfRangeException("Role out of range [0 - 3]");
+		}
+	}
 
-    @Override public String toString()
-    {
-        return "TeamMember{" + "name='" + name + '\'' + ", employeeNumber="
-            + employeeNumber + ", role=" + role + ", birthday=" + birthday
-            + '}';
-    }
+	@Override public String toString()
+	{
+		return "TeamMember{" + "name='" + name + '\'' + ", employeeNumber=" + employeeNumber + ", role=" + role
+			+ ", birthday=" + birthday + '}';
+	}
+
+}
+
+class OutOfRangeException extends RuntimeException
+{
+	public OutOfRangeException(String message)
+	{
+		super(message);
+	}
+
+	@Override public String toString()
+	{
+		return "Role couldn't be set: " + super.getMessage();
+	}
 }
