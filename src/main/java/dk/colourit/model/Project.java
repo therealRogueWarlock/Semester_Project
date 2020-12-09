6a217fb72af6570efe63b5381e6842be86973338
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-09 23:26:41 +0100

    Added getTotalTime

--- src/main/java/dk/colourit/model/Project.java
+++ src/main/java/dk/colourit/model/Project.java
@@ -3,8 +3,7 @@ package dk.colourit.model;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
-public class Project
-{
+public class Project {
 
 	private String name;
 	private MyDate creationDate;
@@ -16,27 +15,20 @@ public class Project
 	private RequirementList requirementList;
 	private TeamMemberList teamMemberList;
 
-	public Project(String name, MyDate startDate, MyDate deadLine)
-	{
+	public Project(String name, MyDate startDate, MyDate deadLine) {
 		this.creationDate = MyDate.now();
 		this.endDate = null;
 		status = 0;
 
-		if (name.isEmpty() || name.isBlank())
-		{
+		if (name.isEmpty() || name.isBlank()) {
 			throw new InvalidParameterException();
-		}
-		else
-		{
+		} else {
 			this.name = name;
 		}
 
-		if (startDate.isBefore(creationDate) || deadLine.isBefore(creationDate) || deadLine.isBefore(startDate))
-		{
+		if (startDate.isBefore(creationDate) || deadLine.isBefore(creationDate) || deadLine.isBefore(startDate)) {
 			throw new InvalidParameterException();
-		}
-		else
-		{
+		} else {
 			this.startDate = startDate;
 			this.deadLine = deadLine;
 		}
@@ -45,66 +37,66 @@ public class Project
 		teamMemberList = new TeamMemberList();
 	}
 
+	public int getTotalTime() {
+		int time = 0;
+		for (Requirement req : requirementList.getRequirements()) {
+			for (Task task : req.getTaskList().getTasks()) {
+				time += task.getTotalTimeSpent();
+			}
+		}
+		return time;
+	}
 
 	public String getStatus() {
 		ArrayList<Requirement> requirements = requirementList.getRequirements();
 
-		int totalRequirements=requirements.size();
+		int totalRequirements = requirements.size();
 
-		int finishedRequirement=0;
+		int finishedRequirement = 0;
 		double procentFinished;
 
-		for (Requirement requirement:requirements)
-		{
-			if(requirement.getStatus().equals("Finished"))
+		for (Requirement requirement : requirements) {
+			if (requirement.getStatus().equals("Finished"))
 				finishedRequirement++;
 		}
 
-		procentFinished = ((double)finishedRequirement/totalRequirements)*100;
+		procentFinished = ((double) finishedRequirement / totalRequirements) * 100;
 
 		return finishedRequirement + "/" + totalRequirements + " requirements finished";
 		//TODO:Lege projekt - Skal fungere ordentligt!
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public MyDate getStartDate()
-	{
+	public MyDate getStartDate() {
 		return startDate;
 	}
 
-	public MyDate getDeadLine()
-	{
+	public MyDate getDeadLine() {
 		return deadLine;
 	}
 
-	public void setRequirementList(RequirementList requirementList)
-	{
+	public void setRequirementList(RequirementList requirementList) {
 		this.requirementList = requirementList;
 		//TODO: Hvad bruges denne funktion til? - SBT - (Tror jeg har forstået det, men stadig lige forklares tak)
 	}
 
-	public RequirementList getRequirementList()
-	{
+	public RequirementList getRequirementList() {
 		return requirementList;
 	}
 
-	public void setTeamMemberList(TeamMemberList teamMemberList)
-	{
+	public void setTeamMemberList(TeamMemberList teamMemberList) {
 		this.teamMemberList = teamMemberList;
 		//TODO: Hvad bruges denne funktion til? - SBT - (Tror jeg har forstået det, men stadig lige forklares tak)
 	}
 
-	public TeamMemberList getTeamMemberList()
-	{
+	public TeamMemberList getTeamMemberList() {
 		return teamMemberList;
 	}
 
-	public void setEndDate(MyDate endDate)
-	{
+	public void setEndDate(MyDate endDate) {
 		this.endDate = endDate;
 		//TODO: endDate = MyDate.now(); - Hvad med dette, når vi færdiggøre projektet via en knap i GUI?
 	}
