326e427a25290d2d916d36508ac2531c03957634
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-10 17:44:53 +0100

    Selected Documentation removed on button click

--- src/main/java/dk/colourit/gui/PopUpController_Documentation.java
+++ src/main/java/dk/colourit/gui/PopUpController_Documentation.java
@@ -8,7 +8,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.time.DateTimeException;
@@ -16,93 +15,105 @@ import java.util.ArrayList;
 
 public class PopUpController_Documentation extends Controller {
 
-    public Label taskName;
-    public Label totalTimeSpent;
+	public Label taskName;
+	public Label totalTimeSpent;
 
-    @FXML private Button logSpentTime;
-    @FXML private Button confirm;
+	@FXML
+	private Button logSpentTime;
+	@FXML
+	private Button confirm;
 
-    @FXML private TextField addTimeSpent;
+	@FXML
+	private TextField addTimeSpent;
 
-    @FXML private ComboBox<TeamMember> selectMember;
+	@FXML
+	private ComboBox<TeamMember> selectMember;
 
-    @FXML private DatePicker selectDate;
+	@FXML
+	private DatePicker selectDate;
 
-    @FXML private CheckBox taskFinished;
+	@FXML
+	private CheckBox taskFinished;
 
-    @FXML private TableView<Documentation> documentationTableView;
-    @FXML private TableColumn<Documentation, String> memberNameColumn;
-    @FXML private TableColumn<Documentation, Integer> timeSpentColumn;
-    @FXML private TableColumn<Documentation, MyDate> daySelectedColumn;
-    @FXML
-    private Label validationLabel;
+	@FXML
+	private TableView<Documentation> documentationTableView;
+	@FXML
+	private TableColumn<Documentation, String> memberNameColumn;
+	@FXML
+	private TableColumn<Documentation, Integer> timeSpentColumn;
+	@FXML
+	private TableColumn<Documentation, MyDate> daySelectedColumn;
+	@FXML
+	private Label validationLabel;
 
-    public void init() {
-        populateComboBox();
-        populateTableView();
+	public void init( ) {
+		populateComboBox( );
+		populateTableView( );
 
+		//Editable false, to make sure user can't make invalid input. Like strings and invalid MyDate data.
+		selectDate.setEditable(false);
+	}
 
-        //Editable false, to make sure user can't make invalid input. Like strings and invalid MyDate data.
-        selectDate.setEditable(false);
-    }
+	// functions for populating data on popup.
 
-public void remove(){
+    private void populateComboBox( ) {
+		selectMember.getItems( ).clear( );
+		ArrayList<TeamMember> teamMembers = ColourItGui.getModel( ).getSelectedProject( ).getTeamMemberList( ).getTeamMembers( );
+		System.out.println(teamMembers);
+		selectMember.getItems( ).addAll(teamMembers);
+	}
+	private void populateTableView( ) {
+		ArrayList<Documentation> documentations = ColourItGui.getModel( ).getSelectedTask( ).getDocumentations( );
 
-        goBack();
-}
+		ObservableList<Documentation> observableDocumentations = FXCollections.observableArrayList( );
 
-    // functions for populating data on popup.
-    private void populateComboBox() {
-        selectMember.getItems().clear();
-        ArrayList<TeamMember> teamMembers = ColourItGui.getModel().getSelectedProject().getTeamMemberList().getTeamMembers();
-        System.out.println(teamMembers);
-        selectMember.getItems().addAll(teamMembers);
-    }
+		observableDocumentations.addAll(documentations);
 
-    private void populateTableView() {
-        ArrayList<Documentation> documentations = ColourItGui.getModel().getSelectedTask().getDocumentations();
+		memberNameColumn.setCellValueFactory(new PropertyValueFactory<>("teamMemberName"));
+		timeSpentColumn.setCellValueFactory(new PropertyValueFactory<>("timeSpent"));
+		daySelectedColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-        ObservableList<Documentation> observableDocumentations = FXCollections.observableArrayList();
+		documentationTableView.setItems(observableDocumentations);
+	}
 
-        observableDocumentations.addAll(documentations);
+	// functions for button functionality
 
-        memberNameColumn.setCellValueFactory(new PropertyValueFactory<>("teamMemberName"));
-        timeSpentColumn.setCellValueFactory(new PropertyValueFactory<>("timeSpent"));
-        daySelectedColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-
-        documentationTableView.setItems(observableDocumentations);
-    }
-
-
-    // functions for button functionality
     @Override
-    public void goBack() {
-        getParentController().init();
-        ((Stage) confirm.getScene().getWindow()).close(); // Get's the Window the button is in, and casts to a Stage, which can be closed with .close()
-    }
+	public void goBack( ) {
+		getParentController( ).init( );
+		( (Stage) confirm.getScene( ).getWindow( ) ).close( ); // Get's the Window the button is in, and casts to a Stage, which can be closed with .close()
+	}
+	public void confirm( ) {
+		ColourItGui.getModel( ).getSelectedTask( ).setFinito(taskFinished.isSelected( ));
+		getParentController( ).getParentController( ).init( );
+		goBack( );
+	}
 
-    public void confirm(){
-        ColourItGui.getModel().getSelectedTask().setFinito(taskFinished.isSelected());
-        getParentController().getParentController().init();
-        goBack();
-    }
+	public void toggleDisabledLogSpentTimeButton( ) {
+		logSpentTime.setDisable(! ( logSpentTime.isDisable( ) ));
+	}
 
-    public void toggleDisabledLogSpentTimeButton(){
-        logSpentTime.setDisable(!(logSpentTime.isDisable()));
-    }
+	@FXML
+	private void addDocumentation( ) {
+		try {
+			TeamMember member = (TeamMember) selectMember.getSelectionModel( ).getSelectedItem( );
+			int timeSpent = Integer.parseInt(addTimeSpent.getText( ));
+			if ( MyDate.now( ).isBefore(new MyDate(selectDate.getValue( ))) )
+				throw new DateTimeException("Date can't be later than current day");
+			MyDate date = new MyDate(selectDate.getValue( ));
+			Documentation doc = new Documentation(member, timeSpent, date);
+			ColourItGui.getModel( ).getSelectedTask( ).getDocumentations( ).add(doc);
+			ColourItGui.getModel().setSelectedDocumentation(doc);
+			init( );
+//			getParentController( ).init( );
+//			getParentController( ).getParentController( ).init( );
+		} catch ( DateTimeException dateTimeException ) {
+			validationLabel.setText("Invalid date");
+		}
+	}
 
-    @FXML
-    private void addDocumentation() {
-        try {TeamMember member = (TeamMember) selectMember.getSelectionModel().getSelectedItem();
-        int timeSpent = Integer.parseInt(addTimeSpent.getText());
-        if ( MyDate.now().isBefore(new MyDate(selectDate.getValue())) ) throw new DateTimeException("Date can't be later than current day");
-        MyDate date = new MyDate(selectDate.getValue());
-        ColourItGui.getModel().getSelectedTask().getDocumentations().add(new Documentation(member, timeSpent, date));
+    public void removeTeamMember( ) {
+        ColourItGui.getModel().getSelectedTask().getDocumentations().remove(documentationTableView.getSelectionModel().getSelectedItem());
         init();
-        getParentController().init();
-        getParentController().getParentController().init();
-    }catch ( DateTimeException dateTimeException ){
-        validationLabel.setText("Invalid date");
-        }
     }
 }
