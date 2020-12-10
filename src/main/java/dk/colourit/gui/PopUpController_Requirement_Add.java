33919f415974135fe5d2b2f3e77a256ee5be4111
AN0NISN4P <andreasyoungandersen@gmail.com>
2020-12-10 17:54:44 +0100

    add requirement validates Duplicate Name FXML is missing a label with fx:id "validationLabel"

--- src/main/java/dk/colourit/gui/PopUpController_Requirement_Add.java
+++ src/main/java/dk/colourit/gui/PopUpController_Requirement_Add.java
@@ -2,11 +2,14 @@ package dk.colourit.gui;
 
 import dk.colourit.model.Project;
 import dk.colourit.model.Requirement;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 
 public class PopUpController_Requirement_Add extends Controller {
 	public TextField requirementNameTextField;
@@ -16,6 +19,8 @@ public class PopUpController_Requirement_Add extends Controller {
 	public TextArea requirementDescriptionTextArea;
 
 	public Button addRequirementButton;
+	@FXML
+	private Label validationLabel;
 
 	@Override
 	public void init( ) {
@@ -23,23 +28,32 @@ public class PopUpController_Requirement_Add extends Controller {
 	}
 
 	public void addRequirement( ) {
-		String requirementNameTextFieldText = requirementNameTextField.getText( );
-		String timeEstimateText = timeEstimateField.getText( );
-		String priorityText;
-		int timeEstimate = Integer.parseInt(timeEstimateText);
-		if ( ! ( priorityTextField.getText( ).isBlank( ) ) )
-			priorityText = priorityTextField.getText( );
-		else
-			priorityText = "0";
-		int priority = Integer.parseInt(priorityText);
-		Project selectedProject = ColourItGui.getModel( ).getSelectedProject( );
 
-		// getting RequirementList from selected project and adding a new requirement.
-		selectedProject.getRequirementList( )
-				.addRequirement(new Requirement(requirementNameTextFieldText, timeEstimate, priority));
+		try {
+			for ( Requirement req : ColourItGui.getModel( ).getSelectedProject( ).getRequirementList( ).getRequirements( ) ) {
+				if ( req.getName( ).equalsIgnoreCase(requirementNameTextField.getText( )) )
+					throw new InputMismatchException("Duplicate Name");
+			}
+			String requirementNameTextFieldText = requirementNameTextField.getText( );
+			String timeEstimateText = timeEstimateField.getText( );
+			String priorityText;
+			int timeEstimate = Integer.parseInt(timeEstimateText);
+			if ( ! ( priorityTextField.getText( ).isBlank( ) ) )
+				priorityText = priorityTextField.getText( );
+			else
+				priorityText = "0";
+			int priority = Integer.parseInt(priorityText);
+			Project selectedProject = ColourItGui.getModel( ).getSelectedProject( );
 
-		clearInputFields( );
-		getParentController( ).init( );
+			// getting RequirementList from selected project and adding a new requirement.
+			selectedProject.getRequirementList( )
+					.addRequirement(new Requirement(requirementNameTextFieldText, timeEstimate, priority));
+
+			clearInputFields( );
+			getParentController( ).init( );
+		} catch ( InputMismatchException inputMismatchException ) {
+			validationLabel.setText("Invalid information");
+		}
 	}
 
 	private void clearInputFields( ) {
@@ -47,6 +61,7 @@ public class PopUpController_Requirement_Add extends Controller {
 		priorityTextField.clear( );
 		timeEstimateField.clear( );
 		requirementDescriptionTextArea.clear( );
+		validationLabel.setText("");
 	}
 
 	@Override
