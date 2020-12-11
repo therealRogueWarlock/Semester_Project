a11e73a6e0956a74c45bcf6442d8a48c45f91994
SanderKirchert <Sanders@post.cybercity.dk>
2020-12-11 10:46:30 +0100

    Added comments to abstract controller

--- src/main/java/dk/colourit/gui/Controller.java
+++ src/main/java/dk/colourit/gui/Controller.java
@@ -27,44 +27,51 @@ public abstract class Controller
 
     public abstract void goBack() throws IOException;
 
+    // creating a popup using the fxml loader.
     public void createPopUp(String fxmlName) throws IOException {
 
-        Scene scene = new Scene(loadFXML(fxmlName));
-        Stage stage = new Stage();
+        // loading scene and initializing controller for scene
+        Parent root = loadFXML(fxmlName);
+        // creating a scene from loaded root
+        Scene scene = new Scene(root);
 
+        Stage stage = new Stage();
+        // setting sage with scene
         stage.setScene(scene);
 
-
-        // when the popUp closes the this controller updates data on scene
+        // When the user closes with X the popup calls init on this controller(Parent), this updates data on scene
         stage.setOnCloseRequest(WindowEvent -> this.init());
+        // Popups will most often have a confirm/add button that calls getParentController.init()
+
 
         // when popup is open primary stage cant be accessed.
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.showAndWait();
-
-    }
-
-    public static void refreshRoot(){
-
     }
 
     public void setParentController(Controller parentController) {
         this.parentController = parentController;
     }
 
+
     public Controller getParentController() {
         return parentController;
     }
 
+
+    // all controllers are able to load fxml to create there own stages(Popups)
     private Parent loadFXML(String fxml) throws IOException {
+
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(ColourItGui.class.getResource(fxml + ".fxml"));
         Parent root = loader.load();
-
         Controller controller = loader.getController();
-
+        // calling init on controller, this will populate data views for scene.
         controller.init();
 
+        // setting the parent controller for the popup
+        // The popup is able to update the parent controller(the scene) when data is added,
+        // giving instant feedback
         controller.setParentController(this);
 
         return root;
