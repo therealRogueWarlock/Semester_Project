241f9309d7db115e05140fb3629ad31c1b283ea6
Andreas Y Andersen <andreasyoungandersen@gmail.com>
2020-12-14 13:27:17 +0100

    Autogenerates ID for Employee if field is left blank

--- src/main/java/dk/colourit/gui/PopUpController_TeamMemberList_Add.java
+++ src/main/java/dk/colourit/gui/PopUpController_TeamMemberList_Add.java
@@ -12,54 +12,56 @@ import java.time.DateTimeException;
 import java.util.InputMismatchException;
 
 public class PopUpController_TeamMemberList_Add extends Controller {
-	@FXML
-	private TextField nameTextField;
-	@FXML
-	private TextField idNumberTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField idNumberTextField;
 
-	@FXML
-	private DatePicker birthdateDatePicker;
-	@FXML
-	private Label validationLabel;
-	@FXML
-	private Button closeButton;
+    @FXML
+    private DatePicker birthdateDatePicker;
+    @FXML
+    private Label validationLabel;
+    @FXML
+    private Button closeButton;
 
-	@Override
-	public void init( ) {
+    @Override
+    public void init() {
 
         /*Editable false, to make sure user can't make invalid input. Like strings
 		and invalid MyDate data.*/
-		birthdateDatePicker.setEditable(false);
-	}
+        birthdateDatePicker.setEditable(false);
+    }
 
-	@FXML
-	private void addTeamMember( ) {
-		try {
+    @FXML
+    private void addTeamMember() {
+        try {
 
-			String teamMemberName = nameTextField.getText();
+            String teamMemberName = nameTextField.getText();
 
-			int employeeIdNumber = Integer.parseInt(idNumberTextField.getText());
+            int employeeIdNumber;
+            if (!idNumberTextField.getText().isBlank())
+                employeeIdNumber = Integer.parseInt(idNumberTextField.getText());
+            else employeeIdNumber = (int) Math.floor(Math.random() * 1000000);
+            if (ColourItGui.getModel().getTeamMemberList()
+                    .getTeamMember("id", Integer.toString(employeeIdNumber)) != null)
+                throw new InputMismatchException("Employee with same id already exists");
 
-			if (ColourItGui.getModel().getTeamMemberList()
-					.getTeamMember("id", Integer.toString(employeeIdNumber)) != null)
-				throw new InputMismatchException("Employee with same id already exists");
+            if (MyDate.now().isBefore(new MyDate(birthdateDatePicker.getValue())))
+                throw new DateTimeException("Birthdate cannot be earlier than current date");
 
-			if (MyDate.now().isBefore(new MyDate(birthdateDatePicker.getValue())))
-				throw new DateTimeException("Birthdate cannot be earlier than current date");
+            MyDate birthday = new MyDate(birthdateDatePicker.getValue());
 
-			MyDate birthday = new MyDate(birthdateDatePicker.getValue());
+            ColourItGui.getModel().addEmployee(teamMemberName, employeeIdNumber, birthday);
 
-			ColourItGui.getModel().addEmployee(teamMemberName, employeeIdNumber, birthday);
+            getParentController().init();
+        } catch (DateTimeException | InputMismatchException exception) {
+            validationLabel.setText(exception.getMessage());
+        }
+    }
 
-			getParentController().init();
-		} catch (DateTimeException | InputMismatchException exception) {
-			validationLabel.setText(exception.getMessage());
-		}
-	}
-
-	@Override
-	public void goBack( ) {
-		getParentController().init();
-		((Stage) closeButton.getScene().getWindow()).close();
-	}
+    @Override
+    public void goBack() {
+        getParentController().init();
+        ((Stage) closeButton.getScene().getWindow()).close();
+    }
 }
