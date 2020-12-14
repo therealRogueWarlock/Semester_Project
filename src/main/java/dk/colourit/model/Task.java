270723bc2e04b98b9815284fd2be283788850cc2
Andreas Y Andersen <andreasyoungandersen@gmail.com>
2020-12-14 14:46:26 +0100

    Task id no longer produce Duplicate numbers

--- src/main/java/dk/colourit/model/Task.java
+++ src/main/java/dk/colourit/model/Task.java
@@ -1,133 +1,153 @@
 package dk.colourit.model;
 
+import dk.colourit.gui.ColourItGui;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Task implements Serializable {
 
-	private final MyDate taskCreationDate;
-	private final ArrayList<Documentation> documentations;
-	private String taskName;
-	private String taskResponsible;
-	private int taskTimeEstimate;
-	private boolean highPriority;
-	private int totalTimeSpent;
-	private String taskDescription;
-	private boolean finito;
-	private int id;
+    private final MyDate taskCreationDate;
+    private final ArrayList<Documentation> documentations;
+    private String taskName;
+    private String taskResponsible;
+    private int taskTimeEstimate;
+    private boolean highPriority;
+    private int totalTimeSpent;
+    private String taskDescription;
+    private boolean finito;
+    private final int id;
 
-	public Task(String taskName, int timeEstimateHour, String teamMemberName, boolean highPriority, String taskDescription) {
-		documentations = new ArrayList<>();
+    public Task(String taskName, int timeEstimateHour, String teamMemberName, boolean highPriority, String taskDescription) {
+        documentations = new ArrayList<>();
 
-		this.highPriority = highPriority;
+        this.highPriority = highPriority;
 
-		setTaskName(taskName);
-		setTaskTimeEstimate(timeEstimateHour);
-		taskResponsible = teamMemberName;
+        setTaskName(taskName);
+        setTaskTimeEstimate(timeEstimateHour);
+        taskResponsible = teamMemberName;
 
-		// initialise default values.
-		taskCreationDate = MyDate.now();
-		setTaskDescription(taskDescription);
-		finito = false;
+        // initialise default values.
+        taskCreationDate = MyDate.now();
+        setTaskDescription(taskDescription);
+        finito = false;
 
-		totalTimeSpent = 0;
-		id = (int) Math.floor(Math.random() * 1000000);
-	}
+        totalTimeSpent = 0;
+        id = generateId();
+    }
 
-	public String getTaskName( ) {
-		return taskName;
-	}
+    public String getTaskName() {
+        return taskName;
+    }
 
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-	public String getTaskResponsible( ) {
-		return taskResponsible;
-	}
+    public String getTaskResponsible() {
+        return taskResponsible;
+    }
 
-	public void setTaskResponsible(String teamMemberName) {
-		taskResponsible = teamMemberName;
-	}
+    public void setTaskResponsible(String teamMemberName) {
+        taskResponsible = teamMemberName;
+    }
 
-	public boolean getPriority( ) {
-		return highPriority;
-	}
+    public boolean getPriority() {
+        return highPriority;
+    }
 
-	public String getTaskDescription( ) {
-		return taskDescription;
-	}
+    public String getTaskDescription() {
+        return taskDescription;
+    }
 
-	public void setTaskDescription(String text) {
-		taskDescription = text;
-	}
+    public void setTaskDescription(String text) {
+        taskDescription = text;
+    }
 
-	public void editTask(String name, int time, String memberName, boolean checked, String taskDescription) {
-		setTaskName(name);
-		setTaskTimeEstimate(time);
-		setTaskResponsible(memberName);
-		highPriority = checked;
-		setTaskDescription(taskDescription);
-	}
+    public void editTask(String name, int time, String memberName, boolean checked, String taskDescription) {
+        setTaskName(name);
+        setTaskTimeEstimate(time);
+        setTaskResponsible(memberName);
+        highPriority = checked;
+        setTaskDescription(taskDescription);
+    }
 
-	public boolean isFinito( ) {
-		return finito;
-	}
+    public boolean isFinito() {
+        return finito;
+    }
 
-	public void setFinito(boolean arg) {
-		finito = arg;
-	}
+    public void setFinito(boolean arg) {
+        finito = arg;
+    }
 
-	public String getTaskStatus( ) {
-		if (finito)
-			return "Finished";
-		else
-			return "Not Finished";
-	}
+    public String getTaskStatus() {
+        if (finito)
+            return "Finished";
+        else
+            return "Not Finished";
+    }
 
-	public boolean isHighPriority( ) {
-		return highPriority;
-	}
+    public boolean isHighPriority() {
+        return highPriority;
+    }
 
-	public int getTaskTimeEstimate( ) {
-		return taskTimeEstimate;
-	}
+    public int getTaskTimeEstimate() {
+        return taskTimeEstimate;
+    }
 
-	private void setTaskTimeEstimate(int taskTimeEstimate) {
-		this.taskTimeEstimate = taskTimeEstimate;
-	}
+    private void setTaskTimeEstimate(int taskTimeEstimate) {
+        this.taskTimeEstimate = taskTimeEstimate;
+    }
 
-	public int getTotalTimeSpent( ) {
-		totalTimeSpent = 0;
-		for (Documentation documentation : documentations) {
-			totalTimeSpent += documentation.getTimeSpentDocumentation();
-		}
-		return totalTimeSpent;
-	}
+    public int getTotalTimeSpent() {
+        totalTimeSpent = 0;
+        for (Documentation documentation : documentations) {
+            totalTimeSpent += documentation.getTimeSpentDocumentation();
+        }
+        return totalTimeSpent;
+    }
 
-	public void setTotalTimeSpent(int totalTimeSpent) {
-		this.totalTimeSpent = totalTimeSpent;
-	}
+    public void setTotalTimeSpent(int totalTimeSpent) {
+        this.totalTimeSpent = totalTimeSpent;
+    }
 
-	public ArrayList<Documentation> getDocumentations( ) {
-		return documentations;
-	}
+    public ArrayList<Documentation> getDocumentations() {
+        return documentations;
+    }
 
-	public void setDocumentation(Documentation documentation) {
-		documentations.add(documentation);
-	}
+    public void setDocumentation(Documentation documentation) {
+        documentations.add(documentation);
+    }
 
-	public MyDate getTaskCreationDate( ) {
-		return taskCreationDate;
-	}
+    public MyDate getTaskCreationDate() {
+        return taskCreationDate;
+    }
 
-	public int getId() {
-		return id;
-	}
+    private int generateId() {
+        boolean valid = true;
+        int idNum = (int) Math.floor(Math.random() * 1000000);
+        do {
+            for (Project project : ColourItGui.getModel().getProjectList().getProjects()) {
+                for (Requirement requirement : project.getRequirementList().getRequirements()) {
+                    for (Task task : requirement.getTaskList().getTasks()) {
+                        if (task.getId() == idNum) {
+                            valid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        } while (valid);
+        return idNum;
+    }
 
-	@Override
-	public String toString( ) {
-		return "Task{" + "name='" + taskName + '\'' + ", responsible='" + taskResponsible + '\'' + ", timeEstimateHour="
-				+ taskTimeEstimate + ", highPriority=" + highPriority + ", totalTimeSpent=" + totalTimeSpent + '}';
-	}
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "name='" + taskName + '\'' + ", responsible='" + taskResponsible + '\'' + ", timeEstimateHour="
+                + taskTimeEstimate + ", highPriority=" + highPriority + ", totalTimeSpent=" + totalTimeSpent + '}';
+    }
 }
